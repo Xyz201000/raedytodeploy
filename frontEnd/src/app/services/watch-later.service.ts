706ce3f014ad291b547cab9ac8cba2cc8c647c9a
Xyz201000 <113905452+Xyz201000@users.noreply.github.com>
2024-02-10 17:55:25 +0000

    Update watch-later.service.ts

--- frontEnd/src/app/services/watch-later.service.ts
+++ frontEnd/src/app/services/watch-later.service.ts
@@ -11,15 +11,15 @@ export class WatchLaterService {
   }
   addToReadLater(userName:any,vlogsId:any)
   {
-    return this.https.get(`http://localhost:8091/dashboard/setFav/${userName}/${vlogsId}`)
+    return this.https.get(`http://vishisht-dashboard.itcblogs.xyz/dashboard/setFav/${userName}/${vlogsId}`)
   }
   showFav(userName:any)
   {
-    return this.https.get(`http://localhost:8091/dashboard/showFav/${userName}`)
+    return this.https.get(`http://vishisht-dashboard.itcblogs.xyz/dashboard/showFav/${userName}`)
   }
   removeFav(userName:any,vlogsId:any)
   {
-    return this.https.get(`http://localhost:8085/dashboard/removeFav/${userName}/${vlogsId}`)
+    return this.https.get(`http://vishisht-dashboard.itcblogs.xyz/dashboard/removeFav/${userName}/${vlogsId}`)
   }
 
 }
